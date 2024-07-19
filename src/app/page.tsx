b73d50d88f8428e4e90eb7374b956122ea714ab2
k <c0p31034@bunkyo.ac.jp>
2024-07-20 01:26:56 +0900

    refactor: Convert page.tsx code to ProfileCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import WorkCard from "@/components/WorkCard";
 import GithubIcon from "../../public/icon/github.svg";
 import { getBlogList } from "@/libs/api";
 import { use } from "react";
+import ProfileCard from "@/components/ProfileCard";
 
 
 export default function Home() {
@@ -15,11 +16,11 @@ export default function Home() {
   console.log(contents)
 
   return (
-    <main className="">
+    <main>
       <Header />
 
       <section className="relative flex items-center h-screen">
-        <h1 className="mx-8 text-5xl z-10 transition-all opacity-90 lg:mx-40 lg:text-6xl ">
+        <h1 className="mx-8 text-5xl z-10 transition-all opacity-90 lg:mx-40 lg:text-6xl">
           Hi 👋
           <br />
           I&apos;m a UI designer in Japan.
@@ -41,23 +42,7 @@ export default function Home() {
         </div>
 
         <div className="py-32 max-w-3xl mx-auto">
-          <div className="flex flex-col gap-5 items-center bg-white/[.05] p-9 md:p-16 rounded-3xl md:flex-row m-6 md:m-0">
-            <CircleAvatar width={80} height={80} />
-            <div className="flex flex-col gap-3">
-              <div className="flex flex-col gap-2">
-                <div className="text-center opacity-90 md:text-start">Kosuke Koizumi</div>
-                <p className="text-xs lg:text-sm opacity-65 font-bold">
-                  FigmaとReactが好きな大学生（休学中）。乗換案内のジョルダン株式会社でUIデザイナー兼コーダーとしてインターン中。主に乗換案内ではなく、鉄道会社や自治体関連の案件に関わることが多い。傍らクラウドワークにも挑戦中。
-                </p>
-              </div>
-              <div className="flex justify-center md:justify-start">
-                <a className="flex items-center gap-1.5" href="https://github.com/mizurest">
-                  <GithubIcon />
-                  <span className="text-xs">Github</span>
-                </a>
-              </div>
-            </div>
-          </div>
+          <ProfileCard />
         </div>
       </section>
 
---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,26 @@
+import GithubIcon from "../../public/icon/github.svg";
+import CircleAvatar from "@/components/CircleAvatar";
+
+const ProfileCard = () => {
+  return (
+    <div className="flex flex-col gap-5 items-center bg-white/[.05] p-9 md:p-16 rounded-3xl md:flex-row m-6 md:m-0">
+      <CircleAvatar width={80} height={80} />
+      <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-2">
+          <div className="text-center opacity-90 md:text-start">Kosuke Koizumi</div>
+          <p className="text-xs lg:text-sm opacity-65 font-bold">
+            FigmaとReactが好きな大学生（休学中）。乗換案内のジョルダン株式会社でUIデザイナー兼コーダーとしてインターン中。主に乗換案内ではなく、鉄道会社や自治体関連の案件に関わることが多い。傍らクラウドワークにも挑戦中。
+          </p>
+        </div>
+        <div className="flex justify-center md:justify-start">
+          <a className="flex items-center gap-1.5" href="https://github.com/mizurest">
+            <GithubIcon />
+            <span className="text-xs">Github</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
