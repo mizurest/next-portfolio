813af5143f6fc62c11a0713823ebd8ecb80eeaf7
k <c0p31034@bunkyo.ac.jp>
2024-07-26 02:13:29 +0900

    chore: installed and configured Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -8,22 +9,22 @@ const inter = Inter({ subsets: ["latin"] });
 const lineSeedJP = localFont({
   src: [
     {
-      path: '../fonts/LINESeedJP_OTF_Th.woff2',
-      weight: '100',
-      style: 'normal',
+      path: "../fonts/LINESeedJP_OTF_Th.woff2",
+      weight: "100",
+      style: "normal",
     },
     {
-      path: '../fonts/LINESeedJP_OTF_Bd.woff2',
-      weight: '700',
-      style: 'normal',
+      path: "../fonts/LINESeedJP_OTF_Bd.woff2",
+      weight: "700",
+      style: "normal",
     },
     {
-      path: '../fonts/LINESeedJP_OTF_Eb.woff2',
-      weight: '900',
-      style: 'normal',
+      path: "../fonts/LINESeedJP_OTF_Eb.woff2",
+      weight: "900",
+      style: "normal",
     },
   ],
-})
+});
 
 export const metadata: Metadata = {
   title: "Kosuke Koizumi",
@@ -38,6 +39,7 @@ export default function RootLayout({
   return (
     <html lang="ja" className="scroll-smooth">
       <body className={lineSeedJP.className}>{children}</body>
+      <GoogleAnalytics gaId={process.env.GA_ID ?? ""} />
     </html>
   );
 }
