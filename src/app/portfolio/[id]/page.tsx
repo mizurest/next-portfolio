febf1c255c8737e8d71d25135c203a7eb911d3a2
k <c0p31034@bunkyo.ac.jp>
2024-07-27 19:01:42 +0900

    feat: Update [id]/page.tsx to use useParams for retrieving article ID

--- src/app/portfolio/[id]/page.tsx
+++ src/app/portfolio/[id]/page.tsx
@@ -1,14 +1,23 @@
+"use client";
+import { useParams } from 'next/navigation';
+
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   // APIから取得する予定
   const themeColor = "#625D80";
-  
+
+  // URLから記事のidを取得
+  const params = useParams()
+  console.log(params.id)
+
+  // TODO: idを使ってAPIから記事のデータを取得する
+
   return (
     <>
-      <Header themeColor={themeColor}/>
-      <main className={`bg-[${themeColor}]`}>
+      <Header themeColor={themeColor} />
+      <main style={{ backgroundColor: themeColor }}>
         <section className="relative flex items-center h-screen">
           <div className="mx-8 lg:mx-40 ">
             <h3 className="text-3xl z-10 transition-all opacity-90 lg:text-4xl">テキストテキストテキストテキスト</h3>
